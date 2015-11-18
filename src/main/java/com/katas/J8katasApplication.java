8147cbc1e3d2cf95382c04e326aec1faf5094315
Piero Divasto <pdivasto@mail.uni-mannheim.de>
2015-11-18 03:33:39 +0100

    First commit

---
+++ src/main/java/com/katas/J8katasApplication.java
@@ -0,0 +1,12 @@
+package com.katas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J8katasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J8katasApplication.class, args);
+    }
+}
